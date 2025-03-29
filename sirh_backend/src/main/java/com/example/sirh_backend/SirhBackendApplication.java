ebe0e62a1800853b962128bef618fc159784b84b
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-03-29 17:36:58 -0400

    feat: Added default route

--- sirh_backend/src/main/java/com/example/sirh_backend/SirhBackendApplication.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/SirhBackendApplication.java
@@ -2,12 +2,21 @@ package com.example.sirh_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SirhBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SirhBackendApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index(@RequestParam(value = "name", defaultValue = "User") String name) {
+        return String.format("Hello %s! Welcome to SIRH.", name);
     }
 
 }
