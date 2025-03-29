0dd3ddd38d2a8e08b6278ccd97625fc66ccc1331
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-03-29 17:37:40 -0400

    feat: Added Employee controller

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/controllers/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.example.sirh_backend.controllers;
+
+import com.example.sirh_backend.models.Employee;
+import com.example.sirh_backend.services.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+}
