9ccfaccee634fa99db85583a8c9eb29ef9d69bcc
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-03-29 13:36:46 -0400

    chore: Initial backend setup

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/SirhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sirh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirhBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SirhBackendApplication.class, args);
+    }
+
+}
