b6ab5e98bd414f5b75f5d955153b325084bc74ca
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-03-29 19:53:06 -0400

    chore: Initial frontend setup

---
+++ sirh_frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>SIRH</h1>
+    </div>
+  )
+}
+
+export default App
