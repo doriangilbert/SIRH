57e653200d3a38ab8c2e87a5c5d04eac77b68b37
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-03-29 17:37:19 -0400

    feat: Added Employee model

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/models/Employee.java
@@ -0,0 +1,43 @@
+package com.example.sirh_backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    public Employee() {}
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
