2c1c40e4e55282daa5c4917505c6c525dad248da
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 12:04:05 -0400

    feat(backend): Added elementary methods for Feedback
    Added :
    - Default constructor
    - Comfort constructor
    - Getters
    - Setters
    - toString method

--- sirh_backend/src/main/java/com/example/sirh_backend/models/Feedback.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/models/Feedback.java
@@ -18,4 +18,50 @@ public class Feedback {
     @ManyToOne
     @JoinColumn(name = "evaluation_id")
     private Evaluation evaluation;
+
+    public Feedback() {}
+
+    public Feedback(String description, Employee reviewer, Evaluation evaluation) {
+        this.description = description;
+        this.reviewer = reviewer;
+        this.evaluation = evaluation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Employee getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(Employee reviewer) {
+        this.reviewer = reviewer;
+    }
+
+    public Evaluation getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(Evaluation evaluation) {
+        this.evaluation = evaluation;
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", reviewer=" + reviewer +
+                ", evaluation=" + evaluation +
+                '}';
+    }
 }
