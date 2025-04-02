99ae9662fed0b7309e082f3d7bf6299b1b437136
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 12:04:34 -0400

    feat(backend): Added elementary methods for Objective
    Added :
    - Default constructor
    - Comfort constructor
    - Getters
    - Setters
    - toString method

--- sirh_backend/src/main/java/com/example/sirh_backend/models/Objective.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/models/Objective.java
@@ -16,4 +16,50 @@ public class Objective {
     @ManyToOne
     @JoinColumn(name = "evaluation_id")
     private Evaluation evaluation;
+
+    public Objective() {}
+
+    public Objective(String description, Evaluation evaluation) {
+        this.description = description;
+        this.achieved = false;
+        this.evaluation = evaluation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAchieved() {
+        return achieved;
+    }
+
+    public void setAchieved(boolean achieved) {
+        this.achieved = achieved;
+    }
+
+    public Evaluation getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(Evaluation evaluation) {
+        this.evaluation = evaluation;
+    }
+
+    @Override
+    public String toString() {
+        return "Objective{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", achieved=" + achieved +
+                ", evaluation=" + evaluation +
+                '}';
+    }
 }
