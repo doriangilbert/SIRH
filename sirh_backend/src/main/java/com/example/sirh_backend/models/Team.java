0e2ffa0fb3722052f39a842ea3a3a2169ccb1f55
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 12:05:21 -0400

    feat(backend): Added elementary methods for Team
    Added :
    - Default constructor
    - Comfort constructor
    - Getters
    - Setters
    - toString method

--- sirh_backend/src/main/java/com/example/sirh_backend/models/Team.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/models/Team.java
@@ -2,6 +2,7 @@ package com.example.sirh_backend.models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -19,4 +20,50 @@ public class Team {
     @ManyToOne
     @JoinColumn(name = "manager_id")
     private Employee manager;
+
+    public Team() {}
+
+    public Team(String name, Employee manager) {
+        this.name = name;
+        this.employees = new ArrayList<>();
+        this.manager = manager;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", employees=" + employees +
+                ", manager=" + manager +
+                '}';
+    }
 }
