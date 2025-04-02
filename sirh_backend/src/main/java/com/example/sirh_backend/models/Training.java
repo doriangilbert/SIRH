02fc24883221d0d2f1bb6af4e95a79852742d0e4
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 12:05:35 -0400

    feat(backend): Added elementary methods for Training
    Added :
    - Default constructor
    - Comfort constructor
    - Getters
    - Setters
    - toString method

--- sirh_backend/src/main/java/com/example/sirh_backend/models/Training.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/models/Training.java
@@ -2,6 +2,7 @@ package com.example.sirh_backend.models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -12,6 +13,8 @@ public class Training {
     private Long id;
 
     private String name;
+
+    private String description;
 
     @ManyToMany
     @JoinTable(
@@ -28,4 +31,60 @@ public class Training {
             inverseJoinColumns = @JoinColumn(name = "skill_id")
     )
     private List<Skill> skills;
+
+    public Training() {}
+
+    public Training(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.employees = new ArrayList<>();
+        this.skills = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    @Override
+    public String toString() {
+        return "Training{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", employees=" + employees +
+                ", skills=" + skills +
+                '}';
+    }
 }
