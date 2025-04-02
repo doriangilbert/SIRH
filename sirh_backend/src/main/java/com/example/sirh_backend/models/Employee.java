087be725ce415863715c5984b5afee4d62510afb
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 12:03:03 -0400

    feat(backend): Added elementary methods for Employee
    Added :
    - Default constructor
    - Comfort constructor
    - Getters
    - Setters
    - toString method

--- sirh_backend/src/main/java/com/example/sirh_backend/models/Employee.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/models/Employee.java
@@ -2,6 +2,7 @@ package com.example.sirh_backend.models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -36,12 +37,16 @@ public class Employee {
     @JoinColumn(name = "team_id")
     private Team team;
 
-    public Employee() {
-    }
+    public Employee() {}
 
-    public Employee(String firstName, String lastName) {
+    public Employee(String firstName, String lastName, Position position, Team team) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.leaveBalance = 0;
+        this.leaves = new ArrayList<>();
+        this.position = position;
+        this.skills = new ArrayList<>();
+        this.team = team;
     }
 
     public Long getId() {
@@ -62,5 +67,59 @@ public class Employee {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getLeaveBalance() {
+        return leaveBalance;
+    }
+
+    public void setLeaveBalance(int leaveBalance) {
+        this.leaveBalance = leaveBalance;
+    }
+
+    public List<Leave> getLeaves() {
+        return leaves;
+    }
+
+    public void setLeaves(List<Leave> leaves) {
+        this.leaves = leaves;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", leaveBalance=" + leaveBalance +
+                ", leaves=" + leaves +
+                ", position=" + position +
+                ", skills=" + skills +
+                ", team=" + team +
+                '}';
     }
 }
