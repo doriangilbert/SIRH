742a142cb333b5fdca4606c9da6f78ad3afdb2ee
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 19:26:34 -0400

    feat(backend): Added Position repository

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/repositories/PositionRepository.java
@@ -0,0 +1,9 @@
+package com.example.sirh_backend.repositories;
+
+import com.example.sirh_backend.models.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PositionRepository extends JpaRepository<Position, Long> {
+}
