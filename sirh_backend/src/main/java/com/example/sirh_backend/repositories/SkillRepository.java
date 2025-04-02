ccac781267b93b6a809bef884e3201a864f7589f
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 19:26:41 -0400

    feat(backend): Added Skill repository

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/repositories/SkillRepository.java
@@ -0,0 +1,9 @@
+package com.example.sirh_backend.repositories;
+
+import com.example.sirh_backend.models.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+}
