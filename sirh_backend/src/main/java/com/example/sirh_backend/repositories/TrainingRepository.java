46f60b2166a7edf622f0c68b258c8027bc081df1
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 19:26:58 -0400

    feat(backend): Added Training repository

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/repositories/TrainingRepository.java
@@ -0,0 +1,9 @@
+package com.example.sirh_backend.repositories;
+
+import com.example.sirh_backend.models.Training;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainingRepository extends JpaRepository<Training, Long> {
+}
