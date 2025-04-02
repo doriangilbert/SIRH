49346aa44c1996dbcde9db92dc1a54a7f6b99620
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 12:03:51 -0400

    feat(backend): Added elementary methods for Evaluation
    Added :
    - Default constructor
    - Comfort constructor
    - Getters
    - Setters
    - toString method

--- sirh_backend/src/main/java/com/example/sirh_backend/models/Evaluation.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/models/Evaluation.java
@@ -3,6 +3,7 @@ package com.example.sirh_backend.models;
 import jakarta.persistence.*;
 
 import java.time.Year;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -28,4 +29,80 @@ public class Evaluation {
 
     @OneToMany(mappedBy = "evaluation", cascade = CascadeType.ALL)
     private List<Objective> objectives;
+
+    public Evaluation() {}
+
+    public Evaluation(Year year, String description, Employee employee) {
+        this.year = year;
+        this.description = description;
+        this.status = EvaluationStatus.WAITING;
+        this.employee = employee;
+        this.feedbacks = new ArrayList<>();
+        this.objectives = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public EvaluationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EvaluationStatus status) {
+        this.status = status;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public List<Feedback> getFeedbacks() {
+        return feedbacks;
+    }
+
+    public void setFeedbacks(List<Feedback> feedbacks) {
+        this.feedbacks = feedbacks;
+    }
+
+    public List<Objective> getObjectives() {
+        return objectives;
+    }
+
+    public void setObjectives(List<Objective> objectives) {
+        this.objectives = objectives;
+    }
+
+    @Override
+    public String toString() {
+        return "Evaluation{" +
+                "id=" + id +
+                ", year=" + year +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", employee=" + employee +
+                ", feedbacks=" + feedbacks +
+                ", objectives=" + objectives +
+                '}';
+    }
 }
