c8e856c057b3757247b9487ba762f944327e945f
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-02 19:25:53 -0400

    feat(backend): Added Feedback repository

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/repositories/FeedbackRepository.java
@@ -0,0 +1,9 @@
+package com.example.sirh_backend.repositories;
+
+import com.example.sirh_backend.models.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+}
