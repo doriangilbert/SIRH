1a45060965fecc5e2ba272874dac0bd33d5f50ef
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-03 19:05:21 -0400

    feat(backend): Added Team controller

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/controllers/TeamController.java
@@ -0,0 +1,33 @@
+package com.example.sirh_backend.controllers;
+
+import com.example.sirh_backend.models.Team;
+import com.example.sirh_backend.services.TeamService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class TeamController {
+
+    private final TeamService teamService;
+
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+    @GetMapping("/teams")
+    public List<Team> getAllTeams() {
+        return teamService.getAllTeams();
+    }
+
+    @GetMapping("/teams/{id}")
+    public Team getTeamById(@PathVariable long id) {
+        return teamService.getTeamById(id);
+    }
+
+    @PostMapping("/teams")
+    public Team createTeam(@RequestBody Team team) {
+        return teamService.createTeam(team);
+    }
+}
