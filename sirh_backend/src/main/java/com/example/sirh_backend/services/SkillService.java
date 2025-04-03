09dd6bc5e075561daa876e25b84597ca8ccae311
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-03 12:53:38 -0400

    feat(backend): Added Skill service

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/services/SkillService.java
@@ -0,0 +1,29 @@
+package com.example.sirh_backend.services;
+
+import com.example.sirh_backend.models.Skill;
+import com.example.sirh_backend.repositories.SkillRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SkillService {
+
+    private final SkillRepository skillRepository;
+
+    public SkillService(SkillRepository skillRepository) {
+        this.skillRepository = skillRepository;
+    }
+
+    public List<Skill> getAllSkills() {
+        return skillRepository.findAll();
+    }
+
+    public Skill getSkillById(long id) {
+        return skillRepository.findById(id).orElse(null);
+    }
+
+    public Skill createSkill(Skill skill) {
+        return skillRepository.save(skill);
+    }
+}
