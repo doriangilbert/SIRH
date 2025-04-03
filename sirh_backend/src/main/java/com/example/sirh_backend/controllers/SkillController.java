8bc4ac0dfee3efcb9fa622293a7ac3add9ca3302
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-03 19:05:14 -0400

    feat(backend): Added Skill controller

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/controllers/SkillController.java
@@ -0,0 +1,33 @@
+package com.example.sirh_backend.controllers;
+
+import com.example.sirh_backend.models.Skill;
+import com.example.sirh_backend.services.SkillService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class SkillController {
+
+    private final SkillService skillService;
+
+    public SkillController(SkillService skillService) {
+        this.skillService = skillService;
+    }
+
+    @GetMapping("/skills")
+    public List<Skill> getAllSkills() {
+        return skillService.getAllSkills();
+    }
+
+    @GetMapping("/skills/{id}")
+    public Skill getSkillById(@PathVariable long id) {
+        return skillService.getSkillById(id);
+    }
+
+    @PostMapping("/skills")
+    public Skill createSkill(@RequestBody Skill skill) {
+        return skillService.createSkill(skill);
+    }
+}
