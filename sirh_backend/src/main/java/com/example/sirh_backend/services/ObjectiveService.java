903b8e2a7c345463ea2e12824d9512256014e3f2
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-03 12:53:20 -0400

    feat(backend): Added Objective service

---
+++ sirh_backend/src/main/java/com/example/sirh_backend/services/ObjectiveService.java
@@ -0,0 +1,29 @@
+package com.example.sirh_backend.services;
+
+import com.example.sirh_backend.models.Objective;
+import com.example.sirh_backend.repositories.ObjectiveRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ObjectiveService {
+
+    private final ObjectiveRepository objectiveRepository;
+
+    public ObjectiveService(ObjectiveRepository objectiveRepository) {
+        this.objectiveRepository = objectiveRepository;
+    }
+
+    public List<Objective> getAllObjectives() {
+        return objectiveRepository.findAll();
+    }
+
+    public Objective getObjectiveById(long id) {
+        return objectiveRepository.findById(id).orElse(null);
+    }
+
+    public Objective createObjective(Objective objective) {
+        return objectiveRepository.save(objective);
+    }
+}
