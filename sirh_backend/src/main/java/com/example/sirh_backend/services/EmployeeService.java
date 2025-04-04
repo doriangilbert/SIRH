28a5762fb9f9280d69954c30381bd0f6e44ea049
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-04 19:52:17 -0400

    refactor(backend): Changed EmployeeService to work with DTO

--- sirh_backend/src/main/java/com/example/sirh_backend/services/EmployeeService.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/services/EmployeeService.java
@@ -1,10 +1,14 @@
 package com.example.sirh_backend.services;
 
+import com.example.sirh_backend.dtos.EmployeeDTO;
 import com.example.sirh_backend.models.Employee;
+import com.example.sirh_backend.models.Leave;
+import com.example.sirh_backend.models.Skill;
 import com.example.sirh_backend.repositories.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
@@ -15,12 +19,36 @@ public class EmployeeService {
         this.employeeRepository = employeeRepository;
     }
 
-    public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
+    public List<EmployeeDTO> getAllEmployees() {
+        return employeeRepository.findAll().stream()
+                .map(employee -> new EmployeeDTO(
+                        employee.getId(),
+                        employee.getFirstName(),
+                        employee.getLastName(),
+                        employee.getLeaveBalance(),
+                        employee.getLeaves().stream().map(Leave::getId).collect(Collectors.toList()),
+                        employee.getPosition().getId(),
+                        employee.getSkills().stream().map(Skill::getId).collect(Collectors.toList()),
+                        employee.getTeam().getId()
+                ))
+                .collect(Collectors.toList());
     }
 
-    public Employee getEmployeeById(long id) {
-        return employeeRepository.findById(id).orElse(null);
+    public EmployeeDTO getEmployeeById(long id) {
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee != null) {
+            return new EmployeeDTO(
+                    employee.getId(),
+                    employee.getFirstName(),
+                    employee.getLastName(),
+                    employee.getLeaveBalance(),
+                    employee.getLeaves().stream().map(Leave::getId).collect(Collectors.toList()),
+                    employee.getPosition().getId(),
+                    employee.getSkills().stream().map(Skill::getId).collect(Collectors.toList()),
+                    employee.getTeam().getId()
+            );
+        }
+        return null;
     }
 
     public Employee createEmployee(Employee employee) {
