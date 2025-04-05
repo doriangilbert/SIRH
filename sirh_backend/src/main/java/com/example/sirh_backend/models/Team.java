d5095edbdd17019cc538f1ead9f9d30ff09a8d48
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-05 18:07:26 -0400

    feat(backend): Added error handling for list methods in Team

--- sirh_backend/src/main/java/com/example/sirh_backend/models/Team.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/models/Team.java
@@ -68,10 +68,18 @@ public class Team {
     }
 
     public void addEmployee(Employee employee) {
-        this.employees.add(employee);
+        if (this.employees.contains(employee)) {
+            throw new IllegalArgumentException("Employee already exists");
+        } else {
+            this.employees.add(employee);
+        }
     }
 
     public void removeEmployee(Employee employee) {
-        this.employees.remove(employee);
+        if (!this.employees.contains(employee)) {
+            throw new IllegalArgumentException("Employee does not exist");
+        } else {
+            this.employees.remove(employee);
+        }
     }
 }
