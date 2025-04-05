033fd39f5767d6bacd57c941fd9ba0cabf8ee697
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-04 23:50:07 -0400

    refactor(backend): Changed TrainingService to work with DTO

--- sirh_backend/src/main/java/com/example/sirh_backend/services/TrainingService.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/services/TrainingService.java
@@ -1,10 +1,14 @@
 package com.example.sirh_backend.services;
 
+import com.example.sirh_backend.dtos.TrainingDTO;
+import com.example.sirh_backend.models.Employee;
+import com.example.sirh_backend.models.Skill;
 import com.example.sirh_backend.models.Training;
 import com.example.sirh_backend.repositories.TrainingRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TrainingService {
@@ -15,12 +19,30 @@ public class TrainingService {
         this.trainingRepository = trainingRepository;
     }
 
-    public List<Training> getAllTrainings() {
-        return trainingRepository.findAll();
+    public List<TrainingDTO> getAllTrainings() {
+        return trainingRepository.findAll().stream()
+                .map(training -> new TrainingDTO(
+                        training.getId(),
+                        training.getName(),
+                        training.getDescription(),
+                        training.getEmployees().stream().map(Employee::getId).collect(Collectors.toList()),
+                        training.getSkills().stream().map(Skill::getId).collect(Collectors.toList())
+                ))
+                .collect(Collectors.toList());
     }
 
-    public Training getTrainingById(long id) {
-        return trainingRepository.findById(id).orElse(null);
+    public TrainingDTO getTrainingById(long id) {
+        Training training = trainingRepository.findById(id).orElse(null);
+        if (training != null) {
+            return new TrainingDTO(
+                    training.getId(),
+                    training.getName(),
+                    training.getDescription(),
+                    training.getEmployees().stream().map(Employee::getId).collect(Collectors.toList()),
+                    training.getSkills().stream().map(Skill::getId).collect(Collectors.toList())
+            );
+        }
+        return null;
     }
 
     public Training createTraining(Training training) {
