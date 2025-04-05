03f9e73a83c1990f10970a7a86221e4e6e24adcd
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-04 23:42:50 -0400

    refactor(backend): Changed TeamService to work with DTO

--- sirh_backend/src/main/java/com/example/sirh_backend/services/TeamService.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/services/TeamService.java
@@ -1,10 +1,13 @@
 package com.example.sirh_backend.services;
 
+import com.example.sirh_backend.dtos.TeamDTO;
+import com.example.sirh_backend.models.Employee;
 import com.example.sirh_backend.models.Team;
 import com.example.sirh_backend.repositories.TeamRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TeamService {
@@ -15,12 +18,28 @@ public class TeamService {
         this.teamRepository = teamRepository;
     }
 
-    public List<Team> getAllTeams() {
-        return teamRepository.findAll();
+    public List<TeamDTO> getAllTeams() {
+        return teamRepository.findAll().stream()
+                .map(team -> new TeamDTO(
+                        team.getId(),
+                        team.getName(),
+                        team.getEmployees().stream().map(Employee::getId).collect(Collectors.toList()),
+                        team.getManager().getId()
+                ))
+                .collect(Collectors.toList());
     }
 
-    public Team getTeamById(long id) {
-        return teamRepository.findById(id).orElse(null);
+    public TeamDTO getTeamById(long id) {
+        Team team = teamRepository.findById(id).orElse(null);
+        if (team != null) {
+            return new TeamDTO(
+                    team.getId(),
+                    team.getName(),
+                    team.getEmployees().stream().map(Employee::getId).collect(Collectors.toList()),
+                    team.getManager().getId()
+            );
+        }
+        return null;
     }
 
     public Team createTeam(Team team) {
