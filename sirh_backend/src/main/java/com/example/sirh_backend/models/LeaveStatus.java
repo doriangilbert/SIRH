514701c731ae8e6490dcf90d4e4d198c864efbef
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-08 17:01:48 -0400

    refactor(backend): Renamed LeaveStatus to RequestStatus

--- sirh_backend/src/main/java/com/example/sirh_backend/models/LeaveStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.sirh_backend.models;
-
-public enum LeaveStatus {
-    APPROVED, // Leave request has been approved
-    REFUSED, // Leave request has been refused
-    PENDING; // Leave request is pending approval
-}
---
+++ sirh_backend/src/main/java/com/example/sirh_backend/models/RequestStatus.java
@@ -0,0 +1,7 @@
+package com.example.sirh_backend.models;
+
+public enum RequestStatus {
+    APPROVED, // Request has been approved
+    REFUSED, // Request has been refused
+    PENDING; // Request is pending approval
+}
