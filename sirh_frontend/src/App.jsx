6386ac42df7542e08bbb449f66fc96328366ff02
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-08 17:04:14 -0400

    refactor(frontend): Renamed Leave to LeaveRequest

--- sirh_frontend/src/App.jsx
+++ sirh_frontend/src/App.jsx
@@ -72,8 +72,8 @@ function App() {
                     <button onClick={() => fetchData('feedbacks')}
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">Feedbacks
                     </button>
-                    <button onClick={() => fetchData('leaves')}
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">Leaves
+                    <button onClick={() => fetchData('leaverequests')}
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">LeaveRequests
                     </button>
                     <button onClick={() => fetchData('objectives')}
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">Objectives
