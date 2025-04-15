a2ca5c129f23f7a67fc33180fef9fa3ec816a91d
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-15 11:10:55 -0400

    refactor(backend): Updated all routes to use DTO and ResponseEntity in TrainingController

--- sirh_backend/src/main/java/com/example/sirh_backend/controllers/TrainingController.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/controllers/TrainingController.java
@@ -4,6 +4,8 @@ import com.example.sirh_backend.dtos.TrainingDTO;
 import com.example.sirh_backend.mappers.TrainingMapper;
 import com.example.sirh_backend.models.entities.Training;
 import com.example.sirh_backend.services.TrainingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,22 +21,22 @@ public class TrainingController {
     }
 
     @GetMapping("/trainings")
-    public List<TrainingDTO> getAllTrainings() {
-        return TrainingMapper.toDTO(trainingService.getAllTrainings());
+    public ResponseEntity<List<TrainingDTO>> getAllTrainings() {
+        return new ResponseEntity<>(TrainingMapper.toDTO(trainingService.getAllTrainings()), HttpStatus.OK);
     }
 
     @GetMapping("/trainings/{id}")
-    public TrainingDTO getTrainingById(@PathVariable long id) {
-        return TrainingMapper.toDTO(trainingService.getTrainingById(id));
+    public ResponseEntity<TrainingDTO> getTrainingById(@PathVariable long id) {
+        return new ResponseEntity<>(TrainingMapper.toDTO(trainingService.getTrainingById(id)), HttpStatus.OK);
     }
 
     @PostMapping("/trainings")
-    public Training createTraining(@RequestBody Training training) {
-        return trainingService.createTraining(training);
+    public ResponseEntity<TrainingDTO> createTraining(@RequestBody Training training) {
+        return new ResponseEntity<>(TrainingMapper.toDTO(trainingService.createTraining(training)), HttpStatus.CREATED);
     }
 
     @PutMapping("/trainings/{id}")
-    public Training updateTraining(@PathVariable long id, @RequestBody Training training) {
-        return trainingService.updateTraining(id, training);
+    public ResponseEntity<TrainingDTO> updateTraining(@PathVariable long id, @RequestBody Training training) {
+        return new ResponseEntity<>(TrainingMapper.toDTO(trainingService.updateTraining(id, training)), HttpStatus.OK);
     }
 }
