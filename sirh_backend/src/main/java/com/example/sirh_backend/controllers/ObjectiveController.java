ce1117fc83bf87f24b4b9044d48c9d27142121c2
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-15 11:10:27 -0400

    refactor(backend): Updated all routes to use DTO and ResponseEntity in ObjectiveController

--- sirh_backend/src/main/java/com/example/sirh_backend/controllers/ObjectiveController.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/controllers/ObjectiveController.java
@@ -4,6 +4,8 @@ import com.example.sirh_backend.dtos.ObjectiveDTO;
 import com.example.sirh_backend.mappers.ObjectiveMapper;
 import com.example.sirh_backend.models.entities.Objective;
 import com.example.sirh_backend.services.ObjectiveService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,22 +21,22 @@ public class ObjectiveController {
     }
 
     @GetMapping("/objectives")
-    public List<ObjectiveDTO> getAllObjectives() {
-        return ObjectiveMapper.toDTO(objectiveService.getAllObjectives());
+    public ResponseEntity<List<ObjectiveDTO>> getAllObjectives() {
+        return new ResponseEntity<>(ObjectiveMapper.toDTO(objectiveService.getAllObjectives()), HttpStatus.OK);
     }
 
     @GetMapping("/objectives/{id}")
-    public ObjectiveDTO getObjectiveById(@PathVariable long id) {
-        return ObjectiveMapper.toDTO(objectiveService.getObjectiveById(id));
+    public ResponseEntity<ObjectiveDTO> getObjectiveById(@PathVariable long id) {
+        return new ResponseEntity<>(ObjectiveMapper.toDTO(objectiveService.getObjectiveById(id)), HttpStatus.OK);
     }
 
     @PostMapping("/objectives")
-    public Objective createObjective(@RequestBody Objective objective) {
-        return objectiveService.createObjective(objective);
+    public ResponseEntity<ObjectiveDTO> createObjective(@RequestBody Objective objective) {
+        return new ResponseEntity<>(ObjectiveMapper.toDTO(objectiveService.createObjective(objective)), HttpStatus.CREATED);
     }
 
     @PutMapping("/objectives/{id}")
-    public Objective updateObjective(@PathVariable long id, @RequestBody Objective objective) {
-        return objectiveService.updateObjective(id, objective);
+    public ResponseEntity<ObjectiveDTO> updateObjective(@PathVariable long id, @RequestBody Objective objective) {
+        return new ResponseEntity<>(ObjectiveMapper.toDTO(objectiveService.updateObjective(id, objective)), HttpStatus.OK);
     }
 }
