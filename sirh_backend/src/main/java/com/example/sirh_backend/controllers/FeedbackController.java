a5e8d15e271c06f89df15c3259c92916a27fce87
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-15 11:09:35 -0400

    refactor(backend): Updated all routes to use DTO and ResponseEntity in FeedbackController

--- sirh_backend/src/main/java/com/example/sirh_backend/controllers/FeedbackController.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/controllers/FeedbackController.java
@@ -4,6 +4,8 @@ import com.example.sirh_backend.dtos.FeedbackDTO;
 import com.example.sirh_backend.mappers.FeedbackMapper;
 import com.example.sirh_backend.models.entities.Feedback;
 import com.example.sirh_backend.services.FeedbackService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,22 +21,22 @@ public class FeedbackController {
     }
 
     @GetMapping("/feedbacks")
-    public List<FeedbackDTO> getAllFeedbacks() {
-        return FeedbackMapper.toDTO(feedbackService.getAllFeedbacks());
+    public ResponseEntity<List<FeedbackDTO>> getAllFeedbacks() {
+        return new ResponseEntity<>(FeedbackMapper.toDTO(feedbackService.getAllFeedbacks()), HttpStatus.OK);
     }
 
     @GetMapping("/feedbacks/{id}")
-    public FeedbackDTO getFeedbackById(@PathVariable long id) {
-        return FeedbackMapper.toDTO(feedbackService.getFeedbackById(id));
+    public ResponseEntity<FeedbackDTO> getFeedbackById(@PathVariable long id) {
+        return new ResponseEntity<>(FeedbackMapper.toDTO(feedbackService.getFeedbackById(id)), HttpStatus.OK);
     }
 
     @PostMapping("/feedbacks")
-    public Feedback createFeedback(@RequestBody Feedback feedback) {
-        return feedbackService.createFeedback(feedback);
+    public ResponseEntity<FeedbackDTO> createFeedback(@RequestBody Feedback feedback) {
+        return new ResponseEntity<>(FeedbackMapper.toDTO(feedbackService.createFeedback(feedback)), HttpStatus.CREATED);
     }
 
     @PutMapping("/feedbacks/{id}")
-    public Feedback updateFeedback(@PathVariable long id, @RequestBody Feedback feedback) {
-        return feedbackService.updateFeedback(id, feedback);
+    public ResponseEntity<FeedbackDTO> updateFeedback(@PathVariable long id, @RequestBody Feedback feedback) {
+        return new ResponseEntity<>(FeedbackMapper.toDTO(feedbackService.updateFeedback(id, feedback)), HttpStatus.OK);
     }
 }
