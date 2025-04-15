9a7b22278d5fb25fc199d06ae4b3a1b0f5dd70ac
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-15 11:09:27 -0400

    refactor(backend): Updated all routes to use DTO and ResponseEntity in EvaluationController

--- sirh_backend/src/main/java/com/example/sirh_backend/controllers/EvaluationController.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/controllers/EvaluationController.java
@@ -4,6 +4,8 @@ import com.example.sirh_backend.dtos.EvaluationDTO;
 import com.example.sirh_backend.mappers.EvaluationMapper;
 import com.example.sirh_backend.models.entities.Evaluation;
 import com.example.sirh_backend.services.EvaluationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,22 +21,22 @@ public class EvaluationController {
     }
 
     @GetMapping("/evaluations")
-    public List<EvaluationDTO> getAllEvaluations() {
-        return EvaluationMapper.toDTO(evaluationService.getAllEvaluations());
+    public ResponseEntity<List<EvaluationDTO>> getAllEvaluations() {
+        return new ResponseEntity<>(EvaluationMapper.toDTO(evaluationService.getAllEvaluations()), HttpStatus.OK);
     }
 
     @GetMapping("/evaluations/{id}")
-    public EvaluationDTO getEvaluationById(@PathVariable long id) {
-        return EvaluationMapper.toDTO(evaluationService.getEvaluationById(id));
+    public ResponseEntity<EvaluationDTO> getEvaluationById(@PathVariable long id) {
+        return new ResponseEntity<>(EvaluationMapper.toDTO(evaluationService.getEvaluationById(id)), HttpStatus.OK);
     }
 
     @PostMapping("/evaluations")
-    public Evaluation createEvaluation(@RequestBody Evaluation evaluation) {
-        return evaluationService.createEvaluation(evaluation);
+    public ResponseEntity<EvaluationDTO> createEvaluation(@RequestBody Evaluation evaluation) {
+        return new ResponseEntity<>(EvaluationMapper.toDTO(evaluationService.createEvaluation(evaluation)), HttpStatus.CREATED);
     }
 
     @PutMapping("/evaluations/{id}")
-    public Evaluation updateEvaluation(@PathVariable long id, @RequestBody Evaluation evaluation) {
-        return evaluationService.updateEvaluation(id, evaluation);
+    public ResponseEntity<EvaluationDTO> updateEvaluation(@PathVariable long id, @RequestBody Evaluation evaluation) {
+        return new ResponseEntity<>(EvaluationMapper.toDTO(evaluationService.updateEvaluation(id, evaluation)), HttpStatus.OK);
     }
 }
