16f6820f18d3b836d37cbf7d0099329338749a14
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-15 11:09:17 -0400

    refactor(backend): Updated all routes to use DTO and ResponseEntity in EmployeeController

--- sirh_backend/src/main/java/com/example/sirh_backend/controllers/EmployeeController.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/controllers/EmployeeController.java
@@ -1,11 +1,18 @@
 package com.example.sirh_backend.controllers;
 
 import com.example.sirh_backend.dtos.EmployeeDTO;
+import com.example.sirh_backend.dtos.LeaveRequestDTO;
+import com.example.sirh_backend.dtos.TrainingRequestDTO;
 import com.example.sirh_backend.mappers.EmployeeMapper;
+import com.example.sirh_backend.mappers.LeaveRequestMapper;
+import com.example.sirh_backend.mappers.TrainingMapper;
+import com.example.sirh_backend.mappers.TrainingRequestMapper;
 import com.example.sirh_backend.models.entities.Employee;
 import com.example.sirh_backend.models.entities.LeaveRequest;
 import com.example.sirh_backend.models.entities.TrainingRequest;
 import com.example.sirh_backend.services.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -22,32 +29,32 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    public List<EmployeeDTO> getAllEmployees() {
-        return EmployeeMapper.toDTO(employeeService.getAllEmployees());
+    public ResponseEntity<List<EmployeeDTO>> getAllEmployees() {
+        return new ResponseEntity<>(EmployeeMapper.toDTO(employeeService.getAllEmployees()), HttpStatus.OK);
     }
 
     @GetMapping("/employees/{id}")
-    public EmployeeDTO getEmployeeById(@PathVariable long id) {
-        return EmployeeMapper.toDTO(employeeService.getEmployeeById(id));
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable long id) {
+        return new ResponseEntity<>(EmployeeMapper.toDTO(employeeService.getEmployeeById(id)), HttpStatus.OK);
     }
 
     @PostMapping("/employees")
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return employeeService.createEmployee(employee);
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<>(EmployeeMapper.toDTO(employeeService.createEmployee(employee)), HttpStatus.CREATED);
     }
 
     @PutMapping("/employees/{id}")
-    public Employee updateEmployee(@PathVariable long id, @RequestBody Employee employee) {
-        return employeeService.updateEmployee(id, employee);
+    public ResponseEntity<EmployeeDTO> updateEmployee(@PathVariable long id, @RequestBody Employee employee) {
+        return new ResponseEntity<>(EmployeeMapper.toDTO(employeeService.updateEmployee(id, employee)), HttpStatus.OK);
     }
 
     @PostMapping("/employees/{id}/leaverequests")
-    public LeaveRequest makeLeaveRequest(@PathVariable long id, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) {
-        return employeeService.makeLeaveRequest(id, LocalDate.parse(startDate), LocalDate.parse(endDate));
+    public ResponseEntity<LeaveRequestDTO> makeLeaveRequest(@PathVariable long id, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) {
+        return new ResponseEntity<>(LeaveRequestMapper.toDTO(employeeService.makeLeaveRequest(id, LocalDate.parse(startDate), LocalDate.parse(endDate))), HttpStatus.CREATED);
     }
 
     @PostMapping("/employees/{id}/trainingrequests")
-    public TrainingRequest makeTrainingRequest(@PathVariable long id, @RequestParam("trainingId") long trainingId) {
-        return employeeService.makeTrainingRequest(trainingId, id);
+    public ResponseEntity<TrainingRequestDTO> makeTrainingRequest(@PathVariable long id, @RequestParam("trainingId") long trainingId) {
+        return new ResponseEntity<>(TrainingRequestMapper.toDTO(employeeService.makeTrainingRequest(trainingId, id)), HttpStatus.CREATED);
     }
 }
