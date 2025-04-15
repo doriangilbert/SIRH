822507ec62137bdf582983ba92f344832556231e
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-15 11:10:35 -0400

    refactor(backend): Updated all routes to use DTO and ResponseEntity in PositionController

--- sirh_backend/src/main/java/com/example/sirh_backend/controllers/PositionController.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/controllers/PositionController.java
@@ -4,6 +4,8 @@ import com.example.sirh_backend.dtos.PositionDTO;
 import com.example.sirh_backend.mappers.PositionMapper;
 import com.example.sirh_backend.models.entities.Position;
 import com.example.sirh_backend.services.PositionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,22 +21,22 @@ public class PositionController {
     }
 
     @GetMapping("/positions")
-    public List<PositionDTO> getAllPositions() {
-        return PositionMapper.toDTO(positionService.getAllPositions());
+    public ResponseEntity<List<PositionDTO>> getAllPositions() {
+        return new ResponseEntity<>(PositionMapper.toDTO(positionService.getAllPositions()), HttpStatus.OK);
     }
 
     @GetMapping("/positions/{id}")
-    public PositionDTO getPositionById(@PathVariable long id) {
-        return PositionMapper.toDTO(positionService.getPositionById(id));
+    public ResponseEntity<PositionDTO> getPositionById(@PathVariable long id) {
+        return new ResponseEntity<>(PositionMapper.toDTO(positionService.getPositionById(id)), HttpStatus.OK);
     }
 
     @PostMapping("/positions")
-    public Position createPosition(@RequestBody Position position) {
-        return positionService.createPosition(position);
+    public ResponseEntity<PositionDTO> createPosition(@RequestBody Position position) {
+        return new ResponseEntity<>(PositionMapper.toDTO(positionService.createPosition(position)), HttpStatus.CREATED);
     }
 
     @PutMapping("/positions/{id}")
-    public Position updatePosition(@PathVariable long id, @RequestBody Position position) {
-        return positionService.updatePosition(id, position);
+    public ResponseEntity<PositionDTO> updatePosition(@PathVariable long id, @RequestBody Position position) {
+        return new ResponseEntity<>(PositionMapper.toDTO(positionService.updatePosition(id, position)), HttpStatus.OK);
     }
 }
