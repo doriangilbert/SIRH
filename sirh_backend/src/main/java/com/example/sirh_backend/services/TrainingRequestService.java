92392a2d44e02bbce57cad94735592f2b8c08289
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-16 12:05:53 -0400

    refactor(backend): Updated error handling for TrainingRequestService

--- sirh_backend/src/main/java/com/example/sirh_backend/services/TrainingRequestService.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/services/TrainingRequestService.java
@@ -1,5 +1,6 @@
 package com.example.sirh_backend.services;
 
+import com.example.sirh_backend.exceptions.NotFoundException;
 import com.example.sirh_backend.models.entities.TrainingRequest;
 import com.example.sirh_backend.models.enums.RequestStatus;
 import com.example.sirh_backend.repositories.TrainingRequestRepository;
@@ -21,7 +22,7 @@ public class TrainingRequestService {
     }
 
     public TrainingRequest getTrainingRequestById(long id) {
-        return trainingRequestRepository.findById(id).orElse(null);
+        return trainingRequestRepository.findById(id).orElseThrow(() -> new NotFoundException("Training request not found"));
     }
 
     public TrainingRequest createTrainingRequest(TrainingRequest trainingRequest) {
@@ -29,56 +30,48 @@ public class TrainingRequestService {
     }
 
     public TrainingRequest updateTrainingRequest(long id, TrainingRequest updatedTrainingRequest) {
-        TrainingRequest trainingRequest = trainingRequestRepository.findById(id).orElse(null);
-        if (trainingRequest != null) {
-            trainingRequest.setStatus(updatedTrainingRequest.getStatus());
-            trainingRequest.setTraining(updatedTrainingRequest.getTraining());
-            trainingRequest.setEmployee(updatedTrainingRequest.getEmployee());
-            trainingRequest.setReviewer(updatedTrainingRequest.getReviewer());
-            return trainingRequestRepository.save(trainingRequest);
+        TrainingRequest trainingRequest = trainingRequestRepository.findById(id).orElseThrow(() -> new NotFoundException("Training request not found"));
+        if (updatedTrainingRequest == null) {
+            throw new IllegalArgumentException("Updated training request data cannot be null");
         }
-        return null;
+        trainingRequest.setStatus(updatedTrainingRequest.getStatus());
+        trainingRequest.setTraining(updatedTrainingRequest.getTraining());
+        trainingRequest.setEmployee(updatedTrainingRequest.getEmployee());
+        trainingRequest.setReviewer(updatedTrainingRequest.getReviewer());
+        return trainingRequestRepository.save(trainingRequest);
     }
 
     public TrainingRequest approveTrainingRequest(long id, long reviewerId) {
-        TrainingRequest trainingRequest = trainingRequestRepository.findById(id).orElse(null);
-        if (trainingRequest != null) {
-            if (trainingRequest.getReviewer().getId() == reviewerId) {
-                trainingRequest.setStatus(RequestStatus.APPROVED);
-                trainingRequest.notifyObservers(
-                        "A training request has been approved.\n" +
-                        "Details:\n" +
-                        "- Training Request ID: " + trainingRequest.getId() + "\n" +
-                        "- Training ID: " + trainingRequest.getTraining().getId() + "\n" +
-                        "- Training Name: " + trainingRequest.getTraining().getName()
-                );
-                return updateTrainingRequest(id, trainingRequest);
-            } else {
-                throw new IllegalStateException("Employee is not the reviewer");
-            }
+        TrainingRequest trainingRequest = trainingRequestRepository.findById(id).orElseThrow(() -> new NotFoundException("Training request not found"));
+        if (trainingRequest.getReviewer().getId() == reviewerId) {
+            trainingRequest.setStatus(RequestStatus.APPROVED);
+            trainingRequest.notifyObservers(
+                    "A training request has been approved.\n" +
+                    "Details:\n" +
+                    "- Training Request ID: " + trainingRequest.getId() + "\n" +
+                    "- Training ID: " + trainingRequest.getTraining().getId() + "\n" +
+                    "- Training Name: " + trainingRequest.getTraining().getName()
+            );
+            return updateTrainingRequest(id, trainingRequest);
         } else {
-            throw new IllegalStateException("Training request not found");
+            throw new IllegalArgumentException("Employee is not the reviewer");
         }
     }
 
     public TrainingRequest refuseTrainingRequest(long id, long reviewerId) {
-        TrainingRequest trainingRequest = trainingRequestRepository.findById(id).orElse(null);
-        if (trainingRequest != null) {
-            if (trainingRequest.getReviewer().getId() == reviewerId) {
-                trainingRequest.setStatus(RequestStatus.REFUSED);
-                trainingRequest.notifyObservers(
-                        "A training request has been refused.\n" +
-                        "Details:\n" +
-                        "- Training Request ID: " + trainingRequest.getId() + "\n" +
-                        "- Training ID: " + trainingRequest.getTraining().getId() + "\n" +
-                        "- Training Name: " + trainingRequest.getTraining().getName()
-                );
-                return updateTrainingRequest(id, trainingRequest);
-            } else {
-                throw new IllegalStateException("Employee is not the reviewer");
-            }
+        TrainingRequest trainingRequest = trainingRequestRepository.findById(id).orElseThrow(() -> new NotFoundException("Training request not found"));
+        if (trainingRequest.getReviewer().getId() == reviewerId) {
+            trainingRequest.setStatus(RequestStatus.REFUSED);
+            trainingRequest.notifyObservers(
+                    "A training request has been refused.\n" +
+                    "Details:\n" +
+                    "- Training Request ID: " + trainingRequest.getId() + "\n" +
+                    "- Training ID: " + trainingRequest.getTraining().getId() + "\n" +
+                    "- Training Name: " + trainingRequest.getTraining().getName()
+            );
+            return updateTrainingRequest(id, trainingRequest);
         } else {
-            throw new IllegalStateException("Training request not found");
+            throw new IllegalArgumentException("Employee is not the reviewer");
         }
     }
 }
