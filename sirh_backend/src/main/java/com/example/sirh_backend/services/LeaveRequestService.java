08853fa929d0447db33ddef9e008a01ea3cfc6ba
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-16 12:05:05 -0400

    refactor(backend): Updated error handling for LeaveRequestService

--- sirh_backend/src/main/java/com/example/sirh_backend/services/LeaveRequestService.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/services/LeaveRequestService.java
@@ -1,5 +1,6 @@
 package com.example.sirh_backend.services;
 
+import com.example.sirh_backend.exceptions.NotFoundException;
 import com.example.sirh_backend.models.entities.LeaveRequest;
 import com.example.sirh_backend.models.enums.RequestStatus;
 import com.example.sirh_backend.repositories.LeaveRequestRepository;
@@ -21,7 +22,7 @@ public class LeaveRequestService {
     }
 
     public LeaveRequest getLeaveRequestById(long id) {
-        return leaveRequestRepository.findById(id).orElse(null);
+        return leaveRequestRepository.findById(id).orElseThrow(() -> new NotFoundException("Leave request not found"));
     }
 
     public LeaveRequest createLeaveRequest(LeaveRequest leaveRequest) {
@@ -29,61 +30,53 @@ public class LeaveRequestService {
     }
 
     public LeaveRequest updateLeaveRequest(long id, LeaveRequest updatedLeaveRequest) {
-        LeaveRequest leaveRequest = leaveRequestRepository.findById(id).orElse(null);
-        if (leaveRequest != null) {
-            leaveRequest.setStartDate(updatedLeaveRequest.getStartDate());
-            leaveRequest.setEndDate(updatedLeaveRequest.getEndDate());
-            leaveRequest.setStatus(updatedLeaveRequest.getStatus());
-            leaveRequest.setEmployee(updatedLeaveRequest.getEmployee());
-            leaveRequest.setReviewer(updatedLeaveRequest.getReviewer());
-            return leaveRequestRepository.save(leaveRequest);
+        LeaveRequest leaveRequest = leaveRequestRepository.findById(id).orElseThrow(() -> new NotFoundException("Leave request not found"));
+        if (updatedLeaveRequest == null) {
+            throw new IllegalArgumentException("Updated leave request data cannot be null");
         }
-        return null;
+        leaveRequest.setStartDate(updatedLeaveRequest.getStartDate());
+        leaveRequest.setEndDate(updatedLeaveRequest.getEndDate());
+        leaveRequest.setStatus(updatedLeaveRequest.getStatus());
+        leaveRequest.setEmployee(updatedLeaveRequest.getEmployee());
+        leaveRequest.setReviewer(updatedLeaveRequest.getReviewer());
+        return leaveRequestRepository.save(leaveRequest);
     }
 
     public LeaveRequest approveLeaveRequest(long id, long reviewerId) {
-        LeaveRequest leaveRequest = leaveRequestRepository.findById(id).orElse(null);
-        if (leaveRequest != null) {
-            if (leaveRequest.getReviewer().getId() == reviewerId) {
-                leaveRequest.setStatus(RequestStatus.APPROVED);
-                leaveRequest.notifyObservers(
-                        "A leave request has been approved.\n" +
-                        "Details:\n" +
-                        "- Leave Request ID: " + leaveRequest.getId() + "\n" +
-                        "- Start Date: " + leaveRequest.getStartDate() + "\n" +
-                        "- End Date: " + leaveRequest.getEndDate()
-                );
-                return updateLeaveRequest(id, leaveRequest);
-            } else {
-                throw new IllegalArgumentException("Employee is not the reviewer");
-            }
+        LeaveRequest leaveRequest = leaveRequestRepository.findById(id).orElseThrow(() -> new NotFoundException("Leave request not found"));
+        if (leaveRequest.getReviewer().getId() == reviewerId) {
+            leaveRequest.setStatus(RequestStatus.APPROVED);
+            leaveRequest.notifyObservers(
+                    "A leave request has been approved.\n" +
+                    "Details:\n" +
+                    "- Leave Request ID: " + leaveRequest.getId() + "\n" +
+                    "- Start Date: " + leaveRequest.getStartDate() + "\n" +
+                    "- End Date: " + leaveRequest.getEndDate()
+            );
+            return updateLeaveRequest(id, leaveRequest);
         } else {
-            throw new IllegalArgumentException("Leave request does not exist");
+            throw new IllegalArgumentException("Employee is not the reviewer");
         }
     }
 
     public LeaveRequest refuseLeaveRequest(long id, long reviewerId) {
-        LeaveRequest leaveRequest = leaveRequestRepository.findById(id).orElse(null);
-        if (leaveRequest != null) {
-            if (leaveRequest.getReviewer().getId() == reviewerId) {
-                leaveRequest.setStatus(RequestStatus.REFUSED);
-                long leaveDuration = (long) leaveRequest.getStartDate().until(leaveRequest.getEndDate()).getDays() + 1;
-                leaveRequest.getEmployee().setLeaveBalance(
-                        leaveRequest.getEmployee().getLeaveBalance() + (int) leaveDuration
-                );
-                leaveRequest.notifyObservers(
-                        "A leave request has been refused.\n" +
-                        "Details:\n" +
-                        "- Leave Request ID: " + leaveRequest.getId() + "\n" +
-                        "- Start Date: " + leaveRequest.getStartDate() + "\n" +
-                        "- End Date: " + leaveRequest.getEndDate()
-                );
-                return updateLeaveRequest(id, leaveRequest);
-            } else {
-                throw new IllegalArgumentException("Employee is not the reviewer");
-            }
+        LeaveRequest leaveRequest = leaveRequestRepository.findById(id).orElseThrow(() -> new NotFoundException("Leave request not found"));
+        if (leaveRequest.getReviewer().getId() == reviewerId) {
+            leaveRequest.setStatus(RequestStatus.REFUSED);
+            long leaveDuration = (long) leaveRequest.getStartDate().until(leaveRequest.getEndDate()).getDays() + 1;
+            leaveRequest.getEmployee().setLeaveBalance(
+                    leaveRequest.getEmployee().getLeaveBalance() + (int) leaveDuration
+            );
+            leaveRequest.notifyObservers(
+                    "A leave request has been refused.\n" +
+                    "Details:\n" +
+                    "- Leave Request ID: " + leaveRequest.getId() + "\n" +
+                    "- Start Date: " + leaveRequest.getStartDate() + "\n" +
+                    "- End Date: " + leaveRequest.getEndDate()
+            );
+            return updateLeaveRequest(id, leaveRequest);
         } else {
-            throw new IllegalArgumentException("Leave request does not exist");
+            throw new IllegalArgumentException("Employee is not the reviewer");
         }
     }
 }
