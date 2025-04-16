498f8ab1b9c22634696ab0e9a871851ee1f85371
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-16 12:04:43 -0400

    refactor(backend): Updated error handling for EmployeeService

--- sirh_backend/src/main/java/com/example/sirh_backend/services/EmployeeService.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/services/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.example.sirh_backend.services;
 
+import com.example.sirh_backend.exceptions.NotFoundException;
 import com.example.sirh_backend.models.entities.*;
 import com.example.sirh_backend.models.patterns.PriorityNotificationDecorator;
 import com.example.sirh_backend.models.patterns.RequestFactory;
@@ -31,7 +32,7 @@ public class EmployeeService {
     }
 
     public Employee getEmployeeById(long id) {
-        return employeeRepository.findById(id).orElse(null);
+        return employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("Employee not found"));
     }
 
     public Employee createEmployee(Employee employee) {
@@ -39,82 +40,41 @@ public class EmployeeService {
     }
 
     public Employee updateEmployee(long id, Employee updatedEmployee) {
-        Employee employee = employeeRepository.findById(id).orElse(null);
-        if (employee != null) {
-            employee.setFirstName(updatedEmployee.getFirstName());
-            employee.setLastName(updatedEmployee.getLastName());
-            employee.setLeaveBalance(updatedEmployee.getLeaveBalance());
-            employee.setPosition(updatedEmployee.getPosition());
-            employee.setSkills(updatedEmployee.getSkills());
-            employee.setTeam(updatedEmployee.getTeam());
-            employee.setNotifications(updatedEmployee.getNotifications());
-            return employeeRepository.save(employee);
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("Employee not found"));
+        if (updatedEmployee == null) {
+            throw new IllegalArgumentException("Updated employee data cannot be null");
         }
-        return null;
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setLeaveBalance(updatedEmployee.getLeaveBalance());
+        employee.setPosition(updatedEmployee.getPosition());
+        employee.setSkills(updatedEmployee.getSkills());
+        employee.setTeam(updatedEmployee.getTeam());
+        employee.setNotifications(updatedEmployee.getNotifications());
+        return employeeRepository.save(employee);
     }
 
     public LeaveRequest makeLeaveRequest(long employeeId, LocalDate startDate, LocalDate endDate) {
-        Employee employee = employeeRepository.findById(employeeId).orElse(null);
-        if (employee != null) {
-            if (!endDate.isBefore(startDate)) {
-                long leaveDuration = (long) startDate.until(endDate).getDays() + 1;
-                if (employee.getLeaveBalance() >= leaveDuration) {
-                    employee.setLeaveBalance((int) (employee.getLeaveBalance() - leaveDuration));
-                    updateEmployee(employeeId, employee);
-                    LeaveRequest leaveRequest = (LeaveRequest) RequestFactory.createRequest("LeaveRequest", startDate, endDate, employee);
-                    leaveRequest.addObserver(employee);
-                    if (employee.getTeam() != null && employee.getTeam().getManager() != null) {
-                        leaveRequest.setReviewer(employee.getTeam().getManager());
-                        leaveRequest.addObserver(employee.getTeam().getManager());
-                        PriorityNotificationDecorator priorityNotification = new PriorityNotificationDecorator(
-                                new Notification(
-                                        "New leave request",
-                                        "You have a new leave request to review.\n" +
-                                                "Details:\n" +
-                                                "- Leave Request ID: " + leaveRequest.getId() + "\n" +
-                                                "- Employee ID: " + employee.getId() + "\n" +
-                                                "- Start Date: " + startDate + "\n" +
-                                                "- End Date: " + endDate,
-                                        employee.getTeam().getManager()));
-                        Notification notification = new Notification(
-                                priorityNotification.getTitle(),
-                                priorityNotification.getDescription(),
-                                priorityNotification.getEmployee());
-                        notificationService.createNotification(notification);
-                    } else {
-                        throw new IllegalStateException("No manager found for the employee's team");
-                    }
-                    return leaveRequestService.createLeaveRequest(leaveRequest);
-                } else {
-                    throw new IllegalStateException("Insufficient leave balance");
-                }
-            } else {
-                throw new IllegalArgumentException("End date must be after start date");
-            }
-        } else {
-            throw new IllegalArgumentException("Employee does not exist");
-        }
-    }
-
-    public TrainingRequest makeTrainingRequest(long trainingId, long employeeId) {
-        Training training = trainingService.getTrainingById(trainingId);
-        Employee employee = employeeRepository.findById(employeeId).orElse(null);
-        if (training != null) {
-            if (employee != null) {
-                TrainingRequest trainingRequest = (TrainingRequest) RequestFactory.createRequest("TrainingRequest", training, employee);
-                trainingRequest.addObserver(employee);
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new NotFoundException("Employee not found"));
+        if (!endDate.isBefore(startDate)) {
+            long leaveDuration = (long) startDate.until(endDate).getDays() + 1;
+            if (employee.getLeaveBalance() >= leaveDuration) {
+                employee.setLeaveBalance((int) (employee.getLeaveBalance() - leaveDuration));
+                updateEmployee(employeeId, employee);
+                LeaveRequest leaveRequest = (LeaveRequest) RequestFactory.createRequest("LeaveRequest", startDate, endDate, employee);
+                leaveRequest.addObserver(employee);
                 if (employee.getTeam() != null && employee.getTeam().getManager() != null) {
-                    trainingRequest.setReviewer(employee.getTeam().getManager());
-                    trainingRequest.addObserver(employee.getTeam().getManager());
+                    leaveRequest.setReviewer(employee.getTeam().getManager());
+                    leaveRequest.addObserver(employee.getTeam().getManager());
                     PriorityNotificationDecorator priorityNotification = new PriorityNotificationDecorator(
                             new Notification(
-                                    "New training request",
-                                    "You have a new training request to review.\n" +
+                                    "New leave request",
+                                    "You have a new leave request to review.\n" +
                                             "Details:\n" +
-                                            "- Training Request ID: " + trainingRequest.getId() + "\n" +
+                                            "- Leave Request ID: " + leaveRequest.getId() + "\n" +
                                             "- Employee ID: " + employee.getId() + "\n" +
-                                            "- Training ID: " + training.getId() + "\n" +
-                                            "- Training Name: " + training.getName(),
+                                            "- Start Date: " + startDate + "\n" +
+                                            "- End Date: " + endDate,
                                     employee.getTeam().getManager()));
                     Notification notification = new Notification(
                             priorityNotification.getTitle(),
@@ -124,12 +84,41 @@ public class EmployeeService {
                 } else {
                     throw new IllegalStateException("No manager found for the employee's team");
                 }
-                return trainingRequestService.createTrainingRequest(trainingRequest);
+                return leaveRequestService.createLeaveRequest(leaveRequest);
             } else {
-                throw new IllegalArgumentException("Employee does not exist");
+                throw new IllegalStateException("Insufficient leave balance");
             }
         } else {
-            throw new IllegalArgumentException("Training does not exist");
+            throw new IllegalArgumentException("End date must be after start date");
         }
+    }
+
+    public TrainingRequest makeTrainingRequest(long trainingId, long employeeId) {
+        Training training = trainingService.getTrainingById(trainingId);
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new NotFoundException("Employee not found"));
+        TrainingRequest trainingRequest = (TrainingRequest) RequestFactory.createRequest("TrainingRequest", training, employee);
+        trainingRequest.addObserver(employee);
+        if (employee.getTeam() != null && employee.getTeam().getManager() != null) {
+            trainingRequest.setReviewer(employee.getTeam().getManager());
+            trainingRequest.addObserver(employee.getTeam().getManager());
+            PriorityNotificationDecorator priorityNotification = new PriorityNotificationDecorator(
+                    new Notification(
+                            "New training request",
+                            "You have a new training request to review.\n" +
+                                    "Details:\n" +
+                                    "- Training Request ID: " + trainingRequest.getId() + "\n" +
+                                    "- Employee ID: " + employee.getId() + "\n" +
+                                    "- Training ID: " + training.getId() + "\n" +
+                                    "- Training Name: " + training.getName(),
+                            employee.getTeam().getManager()));
+            Notification notification = new Notification(
+                    priorityNotification.getTitle(),
+                    priorityNotification.getDescription(),
+                    priorityNotification.getEmployee());
+            notificationService.createNotification(notification);
+        } else {
+            throw new IllegalStateException("No manager found for the employee's team");
+        }
+        return trainingRequestService.createTrainingRequest(trainingRequest);
     }
 }
