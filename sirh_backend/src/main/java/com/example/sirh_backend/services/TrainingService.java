0661d2414610461a5ce145390c816d833b8b63da
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-04-16 12:06:01 -0400

    refactor(backend): Updated error handling for TrainingService

--- sirh_backend/src/main/java/com/example/sirh_backend/services/TrainingService.java
+++ sirh_backend/src/main/java/com/example/sirh_backend/services/TrainingService.java
@@ -1,5 +1,6 @@
 package com.example.sirh_backend.services;
 
+import com.example.sirh_backend.exceptions.NotFoundException;
 import com.example.sirh_backend.models.entities.Training;
 import com.example.sirh_backend.repositories.TrainingRepository;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class TrainingService {
     }
 
     public Training getTrainingById(long id) {
-        return trainingRepository.findById(id).orElse(null);
+        return trainingRepository.findById(id).orElseThrow(() -> new NotFoundException("Training not found"));
     }
 
     public Training createTraining(Training training) {
@@ -28,13 +29,13 @@ public class TrainingService {
     }
 
     public Training updateTraining(long id, Training updatedTraining) {
-        Training training = trainingRepository.findById(id).orElse(null);
-        if (training != null) {
-            training.setName(updatedTraining.getName());
-            training.setDescription(updatedTraining.getDescription());
-            training.setSkills(updatedTraining.getSkills());
-            return trainingRepository.save(training);
+        Training training = trainingRepository.findById(id).orElseThrow(() -> new NotFoundException("Training not found"));
+        if (updatedTraining == null) {
+            throw new IllegalArgumentException("Updated training data cannot be null");
         }
-        return null;
+        training.setName(updatedTraining.getName());
+        training.setDescription(updatedTraining.getDescription());
+        training.setSkills(updatedTraining.getSkills());
+        return trainingRepository.save(training);
     }
 }
